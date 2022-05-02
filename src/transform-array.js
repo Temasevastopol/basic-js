faf1c16391980d0bb3e42816ac4bad7996094fbd
Tema_sevastopol <temasebastopol@yandex.ru>
2022-05-02 18:28:45 +0300

    feat: add solution transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -14,43 +14,41 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 function transform(arr) {
-  console.log(arr)
+  // console.log(arr)
   if (!Array.isArray(arr)){
-    return false
+    throw new Error("'arr' parameter must be an instance of the Array!")
   }
   let res = []
-  for (let i = 0; i <arr.length; i++){
-    console.log(res)
-    if (arr[i] == `--discard-next` && i !== arr.length - 1){
-      i += 1
-      
+  for (let i = 0; i < arr.length; i++){
+    if ( i == 0 && (arr[i] == '--discard-prev' || arr[i] == '--double-prev')){
+                i += 1
+    }    
+    if ( i == arr.length - 1 && (arr[i] == '--double-next' || arr[i] == '--discard-next')){
+                break
     }
-    if (arr[i] == `--discard-next` && (arr[i+2] == '--double-prev' || arr[i+2] == '--discard-prev')){
-      i+=2
-      
+    if (arr[i] == '--discard-next' && (arr[i+2] == '--double-prev' || arr[i+2] == '--discard-prev')){
+                i += 3
+    }
+    if (arr[i] == '--discard-next' && arr[i+2] !== '--double-prev' && arr[i+2] !== '--discard-prev'){
+                i += 2
     }
     if (arr[i] == `--discard-prev`){
-      res.pop()
-      i += 1
-      
+                res.pop()
     }
     if (arr[i] == `--double-next`){
-      arr[i] = arr[i+1]  
-       
+                res.push(arr[i+1])
+                i += 1
     }
-    if (arr[i] == `--double-prev` && i !== 0){
-      arr[i] = arr[i-1]
-      
-    } else {
+    if (arr[i] == `--double-prev`){
+      res.push(arr[i-1])
       i += 1
-      
-    }
-    res.push(arr[i])
-   
+}
+    if (arr[i] !== `--discard-prev`)  {
+                res.push(arr[i])
+    }         
     
   }
-  
-  console.log(res)
+
   return res
   // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
